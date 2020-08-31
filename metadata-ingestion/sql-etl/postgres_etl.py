3f3e667f6ed3324bac4992fb9afb021c37b948b2
Grant Nicholas <grant@spothero.com>
2020-08-31 12:55:44 -0500

    Renames postgresql -> redshift

--- metadata-ingestion/sql-etl/postgres_etl.py
+++ metadata-ingestion/sql-etl/postgres_etl.py
@@ -3,7 +3,7 @@ from common import run
 # See https://docs.sqlalchemy.org/en/13/dialects/postgresql.html#module-sqlalchemy.dialects.postgresql.psycopg2 for more details
 URL = '' # e.g. postgresql+psycopg2://user:password@host:port
 OPTIONS = {"echo": True} # e.g. {"client_encoding": "utf8"}
-PLATFORM = 'postgresql'
+PLATFORM = 'redshift'
 SCHEMA_BLACKLIST = ["_fivetran_staging", "admin", "attribution_testing", "bdrupieski_dev", "brazetest_local", "granttest_segment_data_lake", "information_schema"]
 
 run(URL, OPTIONS, PLATFORM, SCHEMA_BLACKLIST)
