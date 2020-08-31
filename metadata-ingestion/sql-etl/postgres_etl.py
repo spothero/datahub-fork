35936c813e9f41bcc6fb750000757f91318abdcf
Grant Nicholas <grant@spothero.com>
2020-08-31 13:09:20 -0500

    Update redshift schema blacklist to exclude local schemas

--- metadata-ingestion/sql-etl/postgres_etl.py
+++ metadata-ingestion/sql-etl/postgres_etl.py
@@ -4,6 +4,6 @@ from common import run
 URL = '' # e.g. postgresql+psycopg2://user:password@host:port
 OPTIONS = {"echo": True} # e.g. {"client_encoding": "utf8"}
 PLATFORM = 'redshift'
-SCHEMA_BLACKLIST = ["_fivetran_staging", "admin", "attribution_testing", "bdrupieski_dev", "brazetest_local", "granttest_segment_data_lake", "information_schema"]
+SCHEMA_BLACKLIST = ["_fivetran_staging", "admin", "attribution_testing", "bdrupieski_dev", "information_schema", "inventory_local", "maggiehays", "money_squad_test", "pipegen_local", "segment_local", "test_grant", "user_deliverability_local", "weather_local"]
 
 run(URL, OPTIONS, PLATFORM, SCHEMA_BLACKLIST)
